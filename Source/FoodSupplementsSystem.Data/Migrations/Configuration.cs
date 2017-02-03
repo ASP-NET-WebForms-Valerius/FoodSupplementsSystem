04e23222779d5c3fe7cae2ea39c85079e54a0a71
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-03 15:33:03 +0200

    Enabled migrations

---
+++ Source/FoodSupplementsSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,38 @@
+namespace FoodSupplementsSystem.Data.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<FoodSupplementsSystem.Data.FoodSupplementsSystemDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(FoodSupplementsSystem.Data.FoodSupplementsSystemDbContext context)
+        {
+            var user = new User()
+            {
+                UserName = "Ana"
+            };
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
