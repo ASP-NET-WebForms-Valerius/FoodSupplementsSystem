a3c27f03399258ea10d7b955348dc8efebef8773
MikeSteiner <mike.steiner.77@gmail.com>
2017-02-11 00:26:46 +0200

    UnitOfWork add

---
+++ Source/FoodSupplementsSystem.Data/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FoodSupplementsSystem.Data.Repositories;
+using FoodSupplementsSystem.Data.Contracts;
+
+namespace FoodSupplementsSystem.Data
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly FoodSupplementsSystemDbContext context;
+        private SupplementRepository supplementRepository;
+
+        private bool disposed = false;
+
+        public UnitOfWork(FoodSupplementsSystemDbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("An instance of DbContext is required to use this UnitOfWork.", "context");
+            }
+
+            this.context = context;
+        }
+
+        public SupplementRepository SupplementRepository
+        {
+            get
+            {
+                if (this.supplementRepository == null)
+                {
+                    this.supplementRepository = new SupplementRepository(this.context);
+                }
+
+                return this.supplementRepository;
+            }
+        }
+                
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            bool isDisposed = this.disposed;
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    this.context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
