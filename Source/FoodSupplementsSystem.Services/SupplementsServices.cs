fc72c5bfd8cbf64207073c99106400f5bda7bbce
MikeSteiner <mike.steiner.77@gmail.com>
2017-02-11 00:41:37 +0200

    FoodSupplementsSystem.Services project added. SupplementsServices init implementation.

---
+++ Source/FoodSupplementsSystem.Services/SupplementsServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FoodSupplementsSystem.Data.Repositories;
+using FoodSupplementsSystem.Data.Models;
+
+namespace FoodSupplementsSystem.Services
+{
+    public class SupplementsServices
+    {
+        private readonly SupplementRepository supplementRepository;
+
+        public SupplementsServices(SupplementRepository supplementRepository)
+        {
+            this.supplementRepository = supplementRepository;
+        }
+
+        public Supplement GetById(int id)
+        {
+            Supplement supplementToReturn = this.supplementRepository.GetById(id);
+
+            return supplementToReturn;
+        }
+
+        public IEnumerable<Supplement> GetAll()
+        {
+            IEnumerable<Supplement> supplementsToReturn = this.supplementRepository.All();
+
+            return supplementsToReturn;
+        }
+
+        public virtual void Add(Supplement supplement)
+        {
+            this.supplementRepository.Add(supplement);
+
+            this.supplementRepository.SaveChanges();
+        }
+
+        public virtual void Update(Supplement supplement)
+        {
+            this.supplementRepository.Update(supplement);
+
+            this.supplementRepository.SaveChanges();
+        }
+
+        public virtual void Delete(Supplement supplement)
+        {
+            this.supplementRepository.Delete(supplement);
+
+            this.supplementRepository.SaveChanges();
+        }
+
+        public virtual void Delete(int supplementId)
+        {
+            this.supplementRepository.Delete(supplementId);
+
+            this.supplementRepository.SaveChanges();
+        }
+    }
+}
