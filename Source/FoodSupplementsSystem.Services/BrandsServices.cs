c281b37d29d60fc8e0fb3c60538adbd5e809232c
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-15 16:12:32 +0200

    Added BrandsServices

---
+++ Source/FoodSupplementsSystem.Services/BrandsServices.cs
@@ -0,0 +1,49 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using FoodSupplementsSystem.Services.Contracts;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services
+{
+    public class BrandsServices : IBrandsServices
+    {
+        private IRepository<Brand> brands;
+
+        public BrandsServices(IRepository<Brand> brands)
+        {
+            this.brands = brands;
+        }
+
+        public IQueryable<Brand> GetAll()
+        {
+            return this.brands.All();
+        }
+
+        public Brand GetById(int id)
+        {
+            return this.brands.GetById(id);
+        }
+
+        public void Create(Brand brand)
+        {
+            this.brands.Add(brand);
+            this.brands.SaveChanges();
+        }
+
+        public void UpdateById(int id, Brand updateBrand)
+        {
+            var brandToUpdate = this.brands.GetById(id);
+
+            brandToUpdate.Name = updateBrand.Name;
+            brandToUpdate.WebSite = updateBrand.WebSite;
+
+            this.brands.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            this.brands.Delete(id);
+            this.brands.SaveChanges();
+        }
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services/Contracts/IBrandsServices.cs
@@ -0,0 +1,18 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services.Contracts
+{
+    public interface IBrandsServices
+    {
+        IQueryable<Brand> GetAll();
+
+        Brand GetById(int id);
+
+        void UpdateById(int id, Brand updateBrand);
+
+        void DeleteById(int id);
+
+        void Create(Brand brand);
+    }
+}
