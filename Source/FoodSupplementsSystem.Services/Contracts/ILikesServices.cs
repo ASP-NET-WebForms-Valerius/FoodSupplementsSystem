c6857659699e81803819a2f189dea28c12843ae5
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-16 01:20:23 +0200

    Added LikesServices

---
+++ Source/FoodSupplementsSystem.Services/Contracts/ILikesServices.cs
@@ -0,0 +1,13 @@
+﻿using FoodSupplementsSystem.Data.Models;
+
+namespace FoodSupplementsSystem.Services.Contracts
+{
+    public interface ILikesServices
+    {
+        Like GetByAuthorIdAndCommentId(string userId, int commentId);
+
+        void ChangeLike(string userId, int commentId);
+
+        void CreateLike(Like like);
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services/LikesServices.cs
@@ -0,0 +1,37 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using FoodSupplementsSystem.Services.Contracts;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services
+{
+    public class LikesServices : ILikesServices
+    {
+        private IRepository<Like> likes;
+
+        public LikesServices(IRepository<Like> likes)
+        {
+            this.likes = likes;
+        }
+
+        public void ChangeLike(string userId, int commentId)
+        {
+            var like = this.GetByAuthorIdAndCommentId(userId, commentId);
+
+            like.Value = !like.Value;
+
+            this.likes.SaveChanges();
+        }
+
+        public void CreateLike(Like like)
+        {
+            this.likes.Add(like);
+            this.likes.SaveChanges();
+        }
+
+        public Like GetByAuthorIdAndCommentId(string userId, int commentId)
+        {
+            return this.likes.All().FirstOrDefault(x => x.AuthorId == userId && x.CommentId == commentId);
+        }
+    }
+}
