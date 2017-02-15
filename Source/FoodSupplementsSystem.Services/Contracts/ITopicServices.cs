027224f32094aa913a510ff453a9ce67de901cd7
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-15 21:26:27 +0200

    Added TopicsServices

---
+++ Source/FoodSupplementsSystem.Services/Contracts/ITopicServices.cs
@@ -0,0 +1,18 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services.Contracts
+{
+    public interface ITopicsServices
+    {
+        IQueryable<Topic> GetAll();
+
+        Topic GetById(int id);
+
+        void UpdateById(int id, Topic updateBrand);
+
+        void DeleteById(int id);
+
+        void Create(Topic brand);
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services/TopicsServices.cs
@@ -0,0 +1,49 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using FoodSupplementsSystem.Services.Contracts;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services
+{
+    public class TopicsServices : ITopicsServices
+    {
+        private IRepository<Topic> topics;
+
+        public TopicsServices(IRepository<Topic> topics)
+        {
+            this.topics = topics;
+        }
+
+        public IQueryable<Topic> GetAll()
+        {
+            return this.topics.All();
+        }
+
+        public Topic GetById(int id)
+        {
+            return this.topics.GetById(id);
+        }
+
+        public void Create(Topic topic)
+        {
+            this.topics.Add(topic);
+            this.topics.SaveChanges();
+        }
+
+        public void UpdateById(int id, Topic updateTopic)
+        {
+            var topicToUpdate = this.topics.GetById(id);
+
+            topicToUpdate.Name = updateTopic.Name;
+            topicToUpdate.Description = updateTopic.Description;
+
+            this.topics.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            this.topics.Delete(id);
+            this.topics.SaveChanges();
+        }
+    }
+}
