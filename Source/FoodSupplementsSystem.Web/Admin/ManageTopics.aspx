cd370069dcb87a5e2b80a3cc2406652f75e4f332
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-15 21:32:25 +0200

    Added admin part for Topics with edit, delete and insert option

---
+++ Source/FoodSupplementsSystem.Web/Admin/ManageTopics.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ManageTopics.aspx.cs" Inherits="FoodSupplementsSystem.Web.Admin.ManageTopics" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView runat="server" ID="gvTopics"
+        ItemType="FoodSupplementsSystem.Data.Models.Topic"
+        DataKeyNames="Id"
+        SelectMethod="gvTopics_GetData"
+        AutoGenerateColumns="true"
+        AllowPaging="true"
+        PageSize="10"
+        UpdateMethod="gvTopics_UpdateItem"
+        DeleteMethod="gvTopics_DeleteItem">
+        <Columns>
+            <asp:CommandField ShowEditButton="true" ControlStyle-CssClass="btn btn-info" />
+            <asp:CommandField ShowDeleteButton="true" ControlStyle-CssClass="btn btn-info" />
+        </Columns>
+    </asp:GridView>
+    <br/>
+    <h3>Add new Topic</h3>
+    <p>Name:</p>
+    <asp:TextBox runat="server" ID="tbInsertName" />
+    <br/>
+    <br/>
+    <p>Description:</p>
+    <asp:TextBox runat="server" ID="tbInsertDescription" />
+    <asp:Button Text="Insert" ID="btnInsert" runat="server" OnClick="btnInsert_Click" CssClass="btn btn-info" />
+    <asp:Button Text="Cancel" ID="btnCancel" runat="server" OnClick="Button1_Click" CssClass="btn btn-danger" />
+</asp:Content>
---
+++ Source/FoodSupplementsSystem.Web/Admin/ManageTopics.aspx.cs
@@ -0,0 +1,67 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Services.Contracts;
+using Ninject;
+using System;
+using System.Linq;
+using System.Web.UI;
+
+namespace FoodSupplementsSystem.Web.Admin
+{
+    public partial class ManageTopics : Page
+    {
+        [Inject]
+        public ITopicsServices TopicsServices { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // The return type can be changed to IEnumerable, however to support
+        // paging and sorting, the following parameters must be added:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable<Topic> gvTopics_GetData()
+        {
+            return this.TopicsServices.GetAll().OrderBy(x => x.Id);
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void gvTopics_UpdateItem(int id)
+        {
+            var topic = new Topic();
+
+            TryUpdateModel(topic);
+
+            this.TopicsServices.UpdateById(id, topic);
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void gvTopics_DeleteItem(int id)
+        {
+            this.TopicsServices.DeleteById(id);
+            //this.Response.Redirect(this.Request.RawUrl);
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            Topic topicToInsert = new Topic();
+
+            topicToInsert.Name = this.tbInsertName.Text;
+            topicToInsert.Description = this.tbInsertDescription.Text;
+
+            this.TopicsServices.Create(topicToInsert);
+
+            this.tbInsertName.Text = "";
+            this.tbInsertDescription.Text = "";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            this.tbInsertName.Text = "";
+            this.tbInsertDescription.Text = "";
+        }
+    }
+}
