eaf7324aa33d4c1d4b97ff8cff6e0a79cf9a7ba9
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-16 03:59:20 +0200

    Added CommentsServices

---
+++ Source/FoodSupplementsSystem.Services/CommentsServices.cs
@@ -0,0 +1,54 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using FoodSupplementsSystem.Services.Contracts;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services
+{
+    public class CommentsServices : ICommentsServices
+    {
+        private IRepository<Comment> comments;
+
+        public CommentsServices(IRepository<Comment> comments)
+        {
+            this.comments = comments;
+        }
+
+        public IQueryable<Comment> GetAll()
+        {
+            return this.comments.All();
+        }
+
+        public IQueryable<Comment> GetTop(int count)
+        {
+            return this.comments.All().OrderByDescending(x => x.Likes.Count()).Take(count);
+        }
+
+        public Comment GetById(int id)
+        {
+            return this.comments.GetById(id);
+        }
+
+        public void Create(Comment comment)
+        {
+            this.comments.Add(comment);
+            this.comments.SaveChanges();
+        }
+
+        public void UpdateById(int id, Comment updateComment)
+        {
+            var commentToUpdate = this.comments.GetById(id);
+
+            commentToUpdate.TopicId = updateComment.TopicId;
+            commentToUpdate.Content = updateComment.Content;
+
+            this.comments.SaveChanges();
+        }
+
+        public void DeleteById(int id)
+        {
+            this.comments.Delete(id);
+            this.comments.SaveChanges();
+        }
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services/Contracts/ICommentsServices.cs
@@ -0,0 +1,20 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using System.Linq;
+
+namespace FoodSupplementsSystem.Services.Contracts
+{
+    public interface ICommentsServices
+    {
+        IQueryable<Comment> GetTop(int count);
+
+        IQueryable<Comment> GetAll();
+
+        Comment GetById(int id);
+
+        void UpdateById(int id, Comment updateComment);
+
+        void DeleteById(int id);
+
+        void Create(Comment comment);
+    }
+}
