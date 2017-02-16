82dd65cb735b4b94ba812c59b16dae45d3204398
MikeSteiner <mike.steiner.77@gmail.com>
2017-02-16 18:35:15 +0200

    First test project setup FoodSupplementsSystem.Services.Tests.csproj

---
+++ Source/FoodSupplementsSystem.Services.Tests/SupplementsServicesTests/GetById_Should.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+using FoodSupplementsSystem.Data;
+using FoodSupplementsSystem.Data.Models;
+
+namespace FoodSupplementsSystem.Services.Tests.SupplementsServicesTests
+{
+    public class GetById_Should
+    {
+        [Test]
+        public void ReturnNull_WhenIdParameterIsNull()
+        {
+            // Arrange
+            var dbContextMocked = new FoodSupplementsSystemDbContext();
+            var unitOfWorkMocked = new UnitOfWork(dbContextMocked);
+            var supplementsServicesMocked = new SupplementsServices(unitOfWorkMocked.SupplementRepository);
+            int id = -1;
+
+            // Act
+            Supplement supplementResult = supplementsServicesMocked.GetById(id);
+
+            // Assert
+            Assert.AreSame(null, supplementResult);
+        }
+    }
+}
