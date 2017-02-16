81783d1ca741978c6daedaad5e82947b288670ae
MikeSteiner <mike.steiner.77@gmail.com>
2017-02-16 20:32:02 +0200

    Another file add FoodSupplements/Web.config and file Contracts/ISupplementRepository.cs

---
+++ Source/FoodSupplementsSystem.Data/Contracts/ISupplementRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FoodSupplementsSystem.Data.Models;
+
+namespace FoodSupplementsSystem.Data.Repositories.Contracts
+{
+    public interface ISupplementRepository
+    {
+        IEnumerable<Supplement> GetFiltered(string categoryName);
+    }
+}
