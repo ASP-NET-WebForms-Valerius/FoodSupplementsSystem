c6ec539c8f284fd289b264c6f1dfb6dbb7e68214
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-22 04:15:06 +0200

    Added unit tests for CategoriesServices for methods GetAll and GetById

---
+++ Source/FoodSupplementsSystem.Services.Tests/CategoriesServicesTests/GetAll_Should.cs
@@ -0,0 +1,92 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSupplementsSystem.Services.Tests.CategoriesServicesTests
+{
+    [TestFixture]
+    public class GetAll_Should
+    {
+        [Test]
+        public void Invoke_TheRepositoryMethodGetAll_Once()
+        {
+            //Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            //Act
+            IEnumerable<Category> categoryResult = categoriesServices.GetAll();
+
+            //Assert
+            categoriesMock.Verify(c => c.All(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnResult_WhenInvokingRepositoryMethod_GetAll()
+        {
+            //Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+            IEnumerable<Category> expectedResultCollection = new List<Category>();
+
+            categoriesMock.Setup(c => c.All()).Returns(() =>
+              {
+                  return expectedResultCollection;
+              });
+
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            //Act
+            IEnumerable<Category> categoryResult = categoriesServices.GetAll();
+
+            //Assert
+            Assert.That(categoryResult, Is.EqualTo(expectedResultCollection));
+        }
+
+        [Test]
+        public void ReturnResultOfCorrectType()
+        {
+            //Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+
+            categoriesMock.Setup(c => c.All()).Returns(() =>
+            {
+                IEnumerable<Category> expectedResultCollection = new List<Category>();
+                return expectedResultCollection;
+            });
+
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            //Act
+            IEnumerable<Category> categoryResult = categoriesServices.GetAll();
+
+            //Assert
+            Assert.That(categoryResult, Is.InstanceOf<IEnumerable<Category>>());
+        }
+
+        [Test]
+        public void ReturnNull_WhenReposityMethodGetAll_ReturnsNull()
+        {
+            //Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+
+            categoriesMock.Setup(c => c.All()).Returns(() =>
+            {
+                return null;
+            });
+
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            //Act
+            IEnumerable<Category> categoryResult = categoriesServices.GetAll();
+
+            //Assert
+            Assert.IsNull(categoryResult);
+        }
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services.Tests/CategoriesServicesTests/GetById_Should.cs
@@ -0,0 +1,44 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace FoodSupplementsSystem.Services.Tests.CategoriesServicesTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [Test]
+        public void ReturnNull_WhenIdParameterIsInvalid()
+        {
+            // Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            // Act
+            Category categoryResult = categoriesServices.GetById(-1);
+
+            // Assert
+            Assert.IsNull(categoryResult);
+        }
+
+        [Test]
+        public void ReturnCategory_WhenIdIsValid()
+        {
+            //Arrange
+            var categoriesMock = new Mock<IRepository<Category>>();
+            int categoryId = 1;
+            Category category = new Category() { Id = 1, Name = "Category1" };
+
+            categoriesMock.Setup(c => c.GetById(categoryId)).Returns(category);
+
+            CategoriesServices categoriesServices = new CategoriesServices(categoriesMock.Object);
+
+            //Act
+            Category categoryResult = categoriesServices.GetById(categoryId);
+
+            //Assert
+            Assert.AreSame(category, categoryResult);
+        }
+    }
+}
