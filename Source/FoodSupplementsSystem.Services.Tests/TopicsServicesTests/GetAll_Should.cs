59ca627614b888b6bc3c6d4ac1f708c1c1703894
Iliana Vachkova <an_vachkova@yahoo.com>
2017-02-22 04:45:55 +0200

    Added unit tests for TopicsServices for methods GetAll and GetById

---
+++ Source/FoodSupplementsSystem.Services.Tests/TopicsServicesTests/GetAll_Should.cs
@@ -0,0 +1,88 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace FoodSupplementsSystem.Services.Tests.TopicsServicesTests
+{
+    [TestFixture]
+    public class GetAll_Should
+    {
+        [Test]
+        public void Invoke_TheRepositoryMethodGetAll_Once()
+        {
+            //Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            //Act
+            IEnumerable<Topic> topicResult = topicsServices.GetAll();
+
+            //Assert
+            topicsMock.Verify(c => c.All(), Times.Once);
+        }
+
+        [Test]
+        public void ReturnResult_WhenInvokingRepositoryMethod_GetAll()
+        {
+            //Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+            IEnumerable<Topic> expectedResultCollection = new List<Topic>();
+
+            topicsMock.Setup(c => c.All()).Returns(() =>
+              {
+                  return expectedResultCollection;
+              });
+
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            //Act
+            IEnumerable<Topic> topicResult = topicsServices.GetAll();
+
+            //Assert
+            Assert.That(topicResult, Is.EqualTo(expectedResultCollection));
+        }
+
+        [Test]
+        public void ReturnResultOfCorrectType()
+        {
+            //Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+
+            topicsMock.Setup(c => c.All()).Returns(() =>
+            {
+                IEnumerable<Topic> expectedResultCollection = new List<Topic>();
+                return expectedResultCollection;
+            });
+
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            //Act
+            IEnumerable<Topic> topicResult = topicsServices.GetAll();
+
+            //Assert
+            Assert.That(topicResult, Is.InstanceOf<IEnumerable<Topic>>());
+        }
+
+        [Test]
+        public void ReturnNull_WhenReposityMethodGetAll_ReturnsNull()
+        {
+            //Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+
+            topicsMock.Setup(c => c.All()).Returns(() =>
+            {
+                return null;
+            });
+
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            //Act
+            IEnumerable<Topic> topicResult = topicsServices.GetAll();
+
+            //Assert
+            Assert.IsNull(topicResult);
+        }
+    }
+}
---
+++ Source/FoodSupplementsSystem.Services.Tests/TopicsServicesTests/GetById_Should.cs
@@ -0,0 +1,44 @@
+﻿using FoodSupplementsSystem.Data.Models;
+using FoodSupplementsSystem.Data.Repositories.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace FoodSupplementsSystem.Services.Tests.TopicsServicesTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [Test]
+        public void ReturnNull_WhenIdParameterIsInvalid()
+        {
+            // Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            // Act
+            Topic topicResult = topicsServices.GetById(-1);
+
+            // Assert
+            Assert.IsNull(topicResult);
+        }
+
+        [Test]
+        public void ReturnTopic_WhenIdIsValid()
+        {
+            //Arrange
+            var topicsMock = new Mock<IRepository<Topic>>();
+            int topicId = 1;
+            Topic topic = new Topic() { Id = topicId, Name = "Topic1" };
+
+            topicsMock.Setup(c => c.GetById(topicId)).Returns(topic);
+
+            TopicsServices topicsServices = new TopicsServices(topicsMock.Object);
+
+            //Act
+            Topic topicResult = topicsServices.GetById(topicId);
+
+            //Assert
+            Assert.AreSame(topic, topicResult);
+        }
+    }
+}
